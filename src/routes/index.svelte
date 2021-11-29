dbf8c238c334a05cb692ce473e2f9baaf907b95c
burn4live <ericb@linux.com>
2021-11-28 19:00:15 -0700

    added seo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,25 @@
 	import Hero from '$components/hero/Hero.svelte';
 	import Process from '$components/process/Process.svelte';
 	import Services from '$components/services/Services.svelte';
+	import SvelteSeo from 'svelte-seo';
 </script>
+
+<SvelteSeo
+	openGraph={{
+		title: 'Lotus Solutions',
+		description: 'Our Description',
+		url: 'https://www.lotussolutions.io',
+		type: 'website'
+		// images: [
+		// 	{
+		// 		url: 'https://www.example.com/images/og-image.jpg',
+		// 		width: 850,
+		// 		height: 650,
+		// 		alt: 'Og Image Alt'
+		// 	}
+		// ]
+	}}
+/>
 
 <svelte:head>
 	<title>Home</title>
