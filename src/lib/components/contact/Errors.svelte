712a26e38386c3423821064c21a1834f8068f22e
burn4live <ericb@linux.com>
2021-12-10 13:14:04 -0700

    removed ugly dot

--- src/lib/components/contact/Errors.svelte
+++ src/lib/components/contact/Errors.svelte
@@ -7,18 +7,18 @@
 
 {#if showMessage}
 	<div class={responseMessage.type === 'success' ? S.Message : S.MessageError}>
-		<li>{responseMessage.text}</li>
+		{responseMessage.text}
 	</div>
 {/if}
 {#if errors.email && errors.email.required}
-	<div class={S.MessageError}><li>Email is required</li></div>
+	<div class={S.MessageError}>Email is required</div>
 {/if}
 {#if errors.name && errors.name.required}
-	<div class={S.MessageError}><li>Name is required</li></div>
+	<div class={S.MessageError}>Name is required</div>
 {/if}
 {#if errors.subject && errors.subject.required}
-	<div class={S.MessageError}><li>Subject is required</li></div>
+	<div class={S.MessageError}>Subject is required</div>
 {/if}
 {#if errors.message && errors.message.required}
-	<div class={S.MessageError}><li>Message is required</li></div>
+	<div class={S.MessageError}>Message is required</div>
 {/if}
